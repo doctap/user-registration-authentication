0fae696c4d21fcc65c1563ae4e35d302db5b7048
Jo-x <vasilij.alexsandrovich@gmail.com>
2022-11-20 14:14:28 +0600

    Added tableManager, material icon font

--- src/api/http-client.ts
+++ src/api/http-client.ts
@@ -3,3 +3,15 @@ import { users } from "../server.ts/Server"
 export const getUsers = () => {
 	return users;
 }
+
+export const deleteUsers = (usersId: string[]) => {
+	console.log(usersId, 'deleteUsers')
+}
+
+export const blockUsers = (usersId: string[]) => {
+	console.log(usersId, 'blockUsers')
+}
+
+export const unblockUsers = (usersId: string[]) => {
+	console.log(usersId, 'unblockUsers')
+}
--- src/components/pages/pageHome/PageHome.tsx
+++ src/components/pages/pageHome/PageHome.tsx
@@ -1,31 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { convertUsers, User } from '../../../api/convert-data';
-import { getUsers } from '../../../api/http-client';
+import { blockUsers, deleteUsers, getUsers, unblockUsers } from '../../../api/http-client';
+import TableManager, { Actions } from '../../tableManager/TableManager';
 import TableUsers from '../../tableUsers/TableUsers';
 import ContainerPage from '../containerPage/ContainerPage';
 
 export default function PageHome() {
 
-	const [users, setUsers] = useState<User[]>([]);
+	const [users, setUsers] = useState<User[]>(convertUsers(getUsers()));
 
 	const checkedAll = (isChecked: boolean) => {
-		const copy = [...users]
-		copy.forEach(it => isChecked ? it.isChecked = true : it.isChecked = false)
-		setUsers(copy)
+		const items = [...users]
+		items.forEach(it => isChecked ? it.isChecked = true : it.isChecked = false)
+		setUsers(items)
 	}
 
 	const checkedUser = (id: string) => {
-		const copy = [...users]
-		copy.forEach(u => u.id === id && (u.isChecked = !u.isChecked))
-		setUsers(copy)
+		const foundItem = users.find(it => it.id === id);
+
+		if (foundItem === undefined) {
+			console.warn('item is exists but an item not found by id')
+			return;
+		}
+
+		foundItem.isChecked = !foundItem.isChecked;
+		setUsers([...users])
 	}
 
-	useEffect(() => {
-		setUsers(convertUsers(getUsers()))
-	}, [])
+	const getAction = (action: Actions) => {
+		const usersId = users
+			.filter(u => u.isChecked === true)
+			.map(u => u.id);
+
+		if (usersId.length === 0)
+			return;
+
+		switch (action) {
+			case 'block':
+				blockUsers(usersId);
+				break;
+			case 'unblock':
+				unblockUsers(usersId);
+				break;
+			case 'delete':
+				deleteUsers(usersId);
+				break;
+		}
+	}
 
 	return (
 		<ContainerPage variant='padding2rem'>
+			<TableManager getAction={getAction} />
 			<TableUsers checkedAllUsers={checkedAll} checkedUser={checkedUser} users={users} />
 		</ContainerPage>
 	)
